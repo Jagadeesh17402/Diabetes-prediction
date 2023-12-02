2d544537a2141f0b7019c2d08f2ca39a4e55e808
Jagadeesh <91451436+Jagadeesh17402@users.noreply.github.com>
2023-12-02 17:59:19 +0530

    Delete controller directory

--- controller/index_controller.js
+++
@@ -1,6 +0,0 @@
-const { v4: uuidv4 } = require('uuid'); //used to get unique room ids
-
-
-module.exports.joinWhiteboard=function (req,res){
-    return res.render("whiteboard",{ roomId: req.params.room })
-}
