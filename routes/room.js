75bfa0929937ea41d41f4784c951ed3f48d88494
Jagadeesh <91451436+Jagadeesh17402@users.noreply.github.com>
2023-12-02 18:00:34 +0530

    Delete routes directory

--- routes/room.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const indexController = require('../controller/index_controller');
-
-router.get('/whiteboard/:room',indexController.joinWhiteboard);
-
-module.exports = router;
